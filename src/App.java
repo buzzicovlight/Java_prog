5f96317db20ab432ccd9aadca9dba2e903afab86
Buzouski Alex <buzzicovlight@mail.ru>
2017-05-02 20:52:05 +0300

    replace src

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App{           // название класса и название файла идентичны
+	public static void main(String[] args){
+		System.out.println("Hello World!");	   // println - печать и переход на следующую строку
+	}
+}
